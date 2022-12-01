a9483c0dbdc657948a43a0fe6fadfc1a1d9572f3
CheaterCodes <cheater.codes@outlook.com>
2022-12-01 13:47:46 +0100

    Day 1, Part 1

--- day1/src/main.rs
+++ day1/src/main.rs
@@ -1,3 +1,22 @@
-fn main() {
-    println!("Hello, world!");
+use std::{fs::File, error::Error, io::{BufReader, BufRead}};
+
+fn main() -> Result<(), Box<dyn Error>> {
+    let file = File::open("day1/input.txt")?;
+    let reader = BufReader::new(file);
+
+    let mut max = 0;
+    let mut current = 0;
+    
+    for line in reader.lines().map(Result::unwrap) {
+        if let Ok(num) = line.parse::<i32>() {
+            current += num;
+        } else {
+            max = max.max(current);
+            current = 0;
+        }
+    }
+
+    println!("Total of max: {max}");
+
+    Ok(())
 }
