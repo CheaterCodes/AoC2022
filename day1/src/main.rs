f89f6b1fc4bf76f5593f0075c4b53b0e4e0bf0a8
CheaterCodes <cheater.codes@outlook.com>
2022-12-01 13:52:01 +0100

    Day 1, Part 2

--- day1/src/main.rs
+++ day1/src/main.rs
@@ -4,19 +4,24 @@ fn main() -> Result<(), Box<dyn Error>> {
     let file = File::open("day1/input.txt")?;
     let reader = BufReader::new(file);
 
-    let mut max = 0;
+    let mut totals = Vec::new();
     let mut current = 0;
     
     for line in reader.lines().map(Result::unwrap) {
         if let Ok(num) = line.parse::<i32>() {
             current += num;
         } else {
-            max = max.max(current);
+            totals.push(current);
             current = 0;
         }
     }
 
-    println!("Total of max: {max}");
+    totals.sort();
+    totals.reverse();
+
+    println!("Total of max: {}", totals[0]);
+
+    println!("Total of top 3: {}", totals[0] + totals[1] + totals[2]);
 
     Ok(())
 }
