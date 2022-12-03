a1cb1176ba9dd48f7d4043cea6ae56f9505ebed0
CheaterCodes <cheater.codes@outlook.com>
2022-12-03 12:01:07 +0100

    Day 3, Part 2

--- day3/src/main.rs
+++ day3/src/main.rs
@@ -19,17 +19,26 @@ impl Priority for char {
 fn main() -> Result<(), Box<dyn Error>> {
     let file = File::open("day3/input.txt")?;
     let reader = BufReader::new(file);
-    let lines = reader.lines().map(Result::unwrap);
+    let lines = reader.lines().map(Result::unwrap).collect::<Vec<_>>();
 
-    let rucksacks = lines.map(|l| {
-        let parts = l.split_at(l.len() / 2);
-        (parts.0.chars().collect::<Vec<_>>(), parts.1.chars().collect::<Vec<_>>())
-    }).collect::<Vec<_>>();
+    let duplicate_priority_sum = lines.iter()
+        .map(|l| {
+            let parts = l.split_at(l.len() / 2);
+            (parts.0.chars().collect::<Vec<_>>(), parts.1.chars().collect::<Vec<_>>())
+        })
+        .map(|r| r.0.iter().find(|e| r.1.contains(e)).unwrap().priority())
+        .sum::<i32>();
 
+    println!("Sum: {duplicate_priority_sum}");
 
-    let sum: i32 = rucksacks.iter().map(|r| r.0.iter().find(|e| r.1.contains(e)).unwrap().priority()).sum();
-
-    println!("Sum: {sum}");
+    let mut badge_iter = lines.iter();
+    let mut badge_sum = 0;
+    while let (Some(a), Some(b), Some(c)) = (badge_iter.next(), badge_iter.next(), badge_iter.next()) {
+        let badge = a.chars().find(|item| b.contains(*item) && c.contains(*item)).unwrap();
+        badge_sum += badge.priority();
+    }
+    
+    println!("Badge Sum: {badge_sum}");
 
     Ok(())
 }
