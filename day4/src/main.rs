f9398ab2bb45b57955428367102aa6e1c69627cd
CheaterCodes <cheater.codes@outlook.com>
2022-12-04 12:55:04 +0100

    Day 4, Part 1

--- day4/src/main.rs
+++ day4/src/main.rs
@@ -1,9 +1,25 @@
 use std::{fs::File, error::Error, io::{BufReader, BufRead}};
 
 fn main() -> Result<(), Box<dyn Error>> {
-    let file = File::open("day1/input.txt")?;
+    let file = File::open("day4/input.txt")?;
     let reader = BufReader::new(file);
-    let lines = reader.lines().map(Result::unwrap);
+    let lines = reader.lines().map(Result::unwrap).collect::<Vec<_>>();
+
+
+    let ranges = lines.iter()
+        .map(|line| line
+            .splitn(4, ['-', ','])
+            .map(|s| s.parse::<i32>().unwrap())
+            .collect::<Vec<_>>()
+        )
+        .map(|nums| ((nums[0], nums[1]), (nums[2], nums[3])))
+        .collect::<Vec<_>>();
+
+    let number_fully_contained = ranges.iter()
+        .filter(|((min_a, max_a), (min_b, max_b))| min_a >= min_b && max_a <= max_b || min_b >= min_a && max_b <= max_a)
+        .count();
+
+    println!("Fully contains: {number_fully_contained}");
 
     Ok(())
 }
