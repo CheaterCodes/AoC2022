3063f746c82b2d9a12cf174e92b9165f6479a043
CheaterCodes <cheater.codes@outlook.com>
2022-12-06 15:18:04 +0100

    Day 6, Part 1

--- day6/src/main.rs
+++ day6/src/main.rs
@@ -1,9 +1,40 @@
-use std::{error::Error, fs::File, io::{BufReader, BufRead}};
+use std::{error::Error, fs::File, io::{BufReader, BufRead}, collections::VecDeque};
 
 fn main() -> Result<(), Box<dyn Error>> {
-    let file = File::open("day5/input.txt")?;
+    let file = File::open("day6/input.txt")?;
     let reader = BufReader::new(file);
     let lines = reader.lines().map(Result::unwrap).collect::<Vec<_>>();
+    let mut input = lines[0].chars();
+
+    let mut start_marker = VecDeque::new();
+    start_marker.extend((&mut input).take(3));
+
+    let mut taken = 3;
+    for char in &mut input {
+        taken += 1;
+
+        start_marker.push_back(char);
+        if is_marker(start_marker.iter().copied()) {
+            break;
+        }
+
+        start_marker.pop_front();
+    }
+
+    println!("Packet starts at {taken}");
 
     Ok(())
+}
+
+fn is_marker(marker: impl IntoIterator<Item = char>) -> bool {
+    let mut seen = Vec::new();
+    for c in marker {
+        if seen.contains(&c) {
+            return false;
+        }
+
+        seen.push(c);
+    }
+
+    true
 }
