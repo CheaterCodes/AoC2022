295a2d65b2e984adad62edc4895e911bb2248c35
CheaterCodes <cheater.codes@outlook.com>
2022-12-06 14:53:25 +0100

    Created day6 project

---
+++ day6/src/main.rs
@@ -0,0 +1,9 @@
+use std::{error::Error, fs::File, io::{BufReader, BufRead}};
+
+fn main() -> Result<(), Box<dyn Error>> {
+    let file = File::open("day5/input.txt")?;
+    let reader = BufReader::new(file);
+    let lines = reader.lines().map(Result::unwrap).collect::<Vec<_>>();
+
+    Ok(())
+}
