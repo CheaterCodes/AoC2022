b37d79709da07244c6b342cface190288f5e63d1
CheaterCodes <cheater.codes@outlook.com>
2022-12-10 00:32:04 +0100

    Day 8, Part 2

--- day8/src/main.rs
+++ day8/src/main.rs
@@ -1,60 +1,76 @@
 use std::{error::Error, io::{BufReader, BufRead}};
 
+struct Tree {
+    height: usize,
+    view_score: usize,
+    visible: bool
+}
+
 fn main() -> Result<(), Box<dyn Error>> {
     let file = std::fs::File::open("day8/input.txt")?;
     let reader = BufReader::new(file);
     let lines = reader.lines().map(Result::unwrap).collect::<Vec<_>>();
-    let heights = lines.iter().map(|s| s.chars().map(|c| c.to_digit(10).unwrap()).collect::<Vec<_>>()).collect::<Vec<_>>();
+    let mut trees = lines.iter()
+        .map(|s| s
+            .chars()
+            .map(|c| c.to_digit(10).unwrap())
+            .map(|h| Tree {
+                height: h as usize,
+                view_score: 1,
+                visible: false,
+            })
+            .collect::<Vec<_>>()
+        )
+        .collect::<Vec<_>>();
 
-    let height = heights.len();
-    let width = heights[0].len();
-
-    let mut visible = vec![vec![false; width]; height];
+    let height = trees.len();
+    let width = trees[0].len();
 
     for row in 0..height {
-        visible[row][0] = true;
-        visible[row][width - 1] = true;
-
-        let mut max = heights[row][0];
-        for column in 1 .. width - 1 {
-            if heights[row][column] > max {
-                max = heights[row][column];
-                visible[row][column] = true;
-            }
+        let mut last = vec![None; 10];
+        for column in 0..width {
+            let tree = &mut trees[row][column];
+            let last_higher = last[tree.height..].iter().filter_map(|&o| o).max();
+            tree.view_score *= column - last_higher.unwrap_or(0);
+            tree.visible |= last_higher.is_none();
+            last[tree.height] = Some(column);
         }
         
-        let mut max = heights[row][width - 1];
-        for column in (1 .. width - 1).rev() {
-            if heights[row][column] > max {
-                max = heights[row][column];
-                visible[row][column] = true;
-            }
-        }
-    }
-    
-    for column in 0..height {
-        visible[0][column] = true;
-        visible[height - 1][column] = true;
-
-        let mut max = heights[0][column];
-        for row in 1 .. height - 1 {
-            if heights[row][column] > max {
-                max = heights[row][column];
-                visible[row][column] = true;
-            }
-        }
-        
-        let mut max = heights[0][column];
-        for row in (1 .. height - 1).rev() {
-            if heights[row][column] > max {
-                max = heights[row][column];
-                visible[row][column] = true;
-            }
+        let mut last = vec![None; 10];
+        for column in (0..width).rev() {
+            let tree = &mut trees[row][column];
+            let last_higher = last[tree.height..].iter().filter_map(|&o| o).min();
+            tree.view_score *= last_higher.unwrap_or(width - 1) - column;
+            tree.visible |= last_higher.is_none();
+            last[tree.height] = Some(column);
         }
     }
 
-    let visible_count = visible.iter().flatten().filter(|&&v| v).count();
+    for column in 0..width {
+        let mut last = vec![None; 10];
+        for row in 0..height {
+            let tree = &mut trees[row][column];
+            let last_higher = last[tree.height..].iter().filter_map(|&o| o).max();
+            tree.view_score *= row - last_higher.unwrap_or(0);
+            tree.visible |= last_higher.is_none();
+            last[tree.height] = Some(row);
+        }
+        
+        let mut last = vec![None; 10];
+        for row in (0..height).rev() {
+            let tree = &mut trees[row][column];
+            let last_higher = last[tree.height..].iter().filter_map(|&o| o).min();
+            tree.view_score *= last_higher.unwrap_or(height - 1) - row;
+            tree.visible |= last_higher.is_none();
+            last[tree.height] = Some(row);
+        }
+    }
+
+    let visible_count = trees.iter().flatten().filter(|t| t.visible).count();
     println!("Visible: {visible_count}");
+
+    let best = trees.iter().flatten().map(|t| t.view_score).max().unwrap();
+    println!("Best: {best}");
 
     Ok(())
 }
