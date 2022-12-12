28814be9d7fcc94ae9b1aa41464d504dac86f9f5
CheaterCodes <cheater.codes@outlook.com>
2022-12-12 19:40:09 +0100

    Day 10, Part 2

--- day10/src/main.rs
+++ day10/src/main.rs
@@ -33,23 +33,37 @@ fn main() -> Result<(), Box<dyn Error>> {
         }
     });
 
-    let mut cycle = 0;
+    let mut cycle = 1;
     let mut reg_x = 1;
 
     let mut sum_signal = 0;
+    let mut pixels = String::new();
 
     for command in commands {
         for _ in 0..command.cycles() {
-            cycle += 1;
             if (cycle - 20) % 40 == 0 {
                 sum_signal += reg_x * cycle;
             }
+
+            if i32::abs_diff((cycle - 1) % 40, reg_x) <= 1 {
+                pixels.push('\u{2588}');
+            } else {
+                pixels.push(' ');
+            }
+
+            if cycle % 40 == 0 {
+                pixels.push('\n');
+            }
+            
+            cycle += 1;
         }
 
         command.perform(&mut reg_x);
     }
 
     println!("Part 1: {sum_signal}");
+    println!("Part 2:");
+    println!("{pixels}");
 
     Ok(())
 }
