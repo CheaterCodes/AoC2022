e81d058723630670d67d16884c7df09bbd15561a
CheaterCodes <cheater.codes@outlook.com>
2022-12-16 18:30:17 +0100

    Day 15, Part 2

--- day15/src/main.rs
+++ day15/src/main.rs
@@ -39,19 +39,85 @@ fn main() -> Result<(), Box<dyn Error>> {
     }
 
     let min_x = ranges.iter().map(|r| r.0).min().unwrap();
-    let max_x = ranges.iter().map(|r| r.1).max().unwrap();
+    let max_x = ranges.iter().map(|r| r.1).max().unwrap() + 1;
+
     let mut blocked_pos = 0;
-    for pos in min_x..=max_x {
-        if sensors.iter().any(|s| s.beacon == (pos, row)) {
-            continue;
+    let mut pos = (min_x, row);
+    loop {
+        let mut blocked = false;
+        let mut next_x = max_x;
+
+        for sensor in &sensors {
+            let sensor_range = sensor.beacon.0.abs_diff(sensor.position.0) + sensor.beacon.1.abs_diff(sensor.position.1);
+            let sensor_distance = pos.0.abs_diff(sensor.position.0) + pos.1.abs_diff(sensor.position.1);
+            let vertical_distance = pos.1.abs_diff(sensor.position.1);
+
+            if vertical_distance <= sensor_range {
+                if sensor_distance <= sensor_range {
+                    blocked = true;
+                    next_x = max_x.min(sensor.position.0 + (sensor_range as i32 - vertical_distance as i32) + 1);
+                    break;
+                } else {
+                    next_x = next_x.min(sensor.position.0 - (sensor_range as i32 - vertical_distance as i32));
+                }
+            }
         }
 
-        if ranges.iter().any(|range| range.0 <= pos && pos <= range.1) {
-            blocked_pos += 1;
+        if blocked {
+            blocked_pos += next_x - pos.0
+        }
+
+        pos.0 = next_x;
+
+        if pos.0 == max_x {
+            break;
         }
     }
 
+    let mut beacons = sensors.iter().map(|s| s.beacon).collect::<Vec<_>>();
+    beacons.sort();
+    beacons.dedup();
+    blocked_pos -= beacons.iter().filter(|b| b.1 == row).count() as i32;
+
     println!("Part 1: {blocked_pos}");
+
+
+
+    let mut pos = (0i32, 0i32);
+    let max = 4000001;
+    loop {
+        let mut blocked = false;
+        let mut next_x = max;
+
+        for sensor in &sensors {
+            let sensor_range = sensor.beacon.0.abs_diff(sensor.position.0) + sensor.beacon.1.abs_diff(sensor.position.1);
+            let sensor_distance = pos.0.abs_diff(sensor.position.0) + pos.1.abs_diff(sensor.position.1);
+            let vertical_distance = pos.1.abs_diff(sensor.position.1);
+
+            if vertical_distance <= sensor_range {
+                if sensor_distance <= sensor_range {
+                    blocked = true;
+                    next_x = max.min(sensor.position.0 + (sensor_range as i32 - vertical_distance as i32) + 1);
+                    break;
+                } else {
+                    next_x = next_x.min(sensor.position.0 - (sensor_range as i32 - vertical_distance as i32));
+                }
+            }
+        }
+        
+        if !blocked {
+            break;
+        }
+
+        if next_x < max {
+            pos.0 = next_x;
+        } else {
+            pos = (0, pos.1 + 1);
+        }
+    }
+    
+    let frequency = pos.0 as i64 * 4000000 + pos.1 as i64;
+    println!("Part 2: {frequency}");
 
     Ok(())
 }
